e2e15d70606469c787fee34ec4ffec96d4734f33
suranshbaral <suransh567@gmail.com>
2024-03-18 23:02:53 +0545

    ye

--- src/components/ResumeButton.jsx
+++ src/components/ResumeButton.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const ResumeButton = () => {
   const viewResume = () => {
     // Replace 'path/to/your/resume.pdf' with the actual path to your resume PDF
-    const resumePath = './assets/Rupesh_Baral resume.pdf';
+    const resumePath = 'https://drive.google.com/file/d/1SuUAR2xYJbgGJa1K0hEOWai84fbgBD0I/view?usp=sharing';
 
     // Open the resume in a new tab/window
     window.open(resumePath, '_blank');
